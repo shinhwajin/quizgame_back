3223b836b00356d1e8212c48f7487c407f621612
신화진 <tlsghkwlszz55@gmail.com>
2025-12-02 15:42:27 +0900

    feat(user): 로그인,로그아웃 api 생성

--- src/main/java/com/mini/quizgame/api/user/UserApiController.java
+++ src/main/java/com/mini/quizgame/api/user/UserApiController.java
@@ -6,12 +6,15 @@ import com.mini.quizgame.common.GlobalException;
 import com.mini.quizgame.domain.User;
 import com.mini.quizgame.dto.user.LoginForm;
 import com.mini.quizgame.dto.user.UserForm;
+import com.mini.quizgame.repository.user.UserRepository;
 import com.mini.quizgame.service.user.CreateUserService;
 import com.mini.quizgame.service.user.LoginService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserApiController {
 
     private final CreateUserService createUserService;
+    private final UserRepository userRepository;
     private final LoginService loginService;
 
     /**
@@ -29,7 +33,7 @@ public class UserApiController {
      */
     @PostMapping("/join")
     public ResponseEntity<ApiResponse<?>> createUser(@RequestBody @Valid UserForm form) {
-        return ResponseEntity.ok(ApiResponse.success(createUserService.createUser(form)));
+        return ResponseEntity.ok(ApiResponse.success(createUserService.createUser(form), "회원가입 성공"));
     }
 
     /**
@@ -41,19 +45,38 @@ public class UserApiController {
         User loginUser = loginService.login(form.getUserName(), form.getPassword());
         session.setAttribute("LOGIN_USER", loginUser.getId());
 
-        return ResponseEntity.ok(ApiResponse.success("로그인 성공"));
+        return ResponseEntity.ok(ApiResponse.success(loginUser.getId(), "로그인 성공"));
     }
 
-    @GetMapping("/auth")
-    public ResponseEntity<String> main(HttpSession session) {
+    /**
+     * 로그아웃
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<ApiResponse<?>> logout(HttpSession session, HttpServletResponse response) {
+        session.invalidate();
+        ResponseCookie cookie = ResponseCookie.from("JSESSIONID", "")
+                .path("/")
+                .maxAge(0)      // 즉시 만료
+                .httpOnly(true)
+                .build();
 
+        response.addHeader("Set-Cookie", cookie.toString());
+        return ResponseEntity.ok(ApiResponse.success(null, "로그아웃 성공"));
+    }
+
+    /**
+     * 권한 체크
+     */
+    @GetMapping("/check-auth")
+    public ResponseEntity<ApiResponse<?>> checkAuth(HttpSession session) {
         Long loginUserId = (Long) session.getAttribute("LOGIN_USER");
-
         if (loginUserId == null) {
             throw new GlobalException(ErrorCode.UNAUTHORIZED);
         }
 
-        return ResponseEntity.ok("마이페이지 조회 성공");
-    }
+        User user = userRepository.findById(loginUserId)
+                .orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
 
+        return ResponseEntity.ok(ApiResponse.success(user.getId(), "권한 있음"));
+    }
 }
--- src/main/java/com/mini/quizgame/common/ApiResponse.java
+++ src/main/java/com/mini/quizgame/common/ApiResponse.java
@@ -11,8 +11,8 @@ public class ApiResponse<T> {
     private T data;
     private String message;
 
-    public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, data, null);
+    public static <T> ApiResponse<T> success(T data, String message) {
+        return new ApiResponse<>(true, data, message);
     }
 
     public static <T> ApiResponse<T> error(String message) {
